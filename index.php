3ca0d2da9c7f35dacf7eff2c66f819061b7c0ebf
Gabriele Biolo <60276263+gabrielebiolo@users.noreply.github.com>
2021-11-18 19:24:31 +0100

    Primi passi - Parallax
    Iniziamo il sito buttando con un po' di parallax.

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IIS Silvio Ceccato - Classe 5BI</title>
+
+    <?php require("components/head.php"); ?>
+</head>
+<body>
+    <?php require("components/navbar.php") ?>
+
+    <style>
+
+</style>
+
+<div class="parallax-image bgimg-1">
+    <div class="text-on-image">
+        <span class="border">CLASSE 5BI</span>
+    </div>
+</div>
+<div class="mydiv p-10">
+    <p class="mytext">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus maiores distinctio eius, molestiae labore deserunt sed aliquid optio ipsa accusamus tempora ut non similique sunt dicta, pariatur laudantium aperiam facilis?</p>
+</div>
+<div class="parallax-image bgimg-2">
+    <div class="text-on-image">
+        <span class="border">CLASSE 5BI</span>
+    </div>
+</div>
+    
+</body>
+</html>
