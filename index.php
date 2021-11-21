38729a3be015b842087638a49d76b75c37937198
Gabriele Biolo <60276263+gabrielebiolo@users.noreply.github.com>
2021-11-21 23:50:56 +0100

    Aggiunto header di sezione centrale

--- index.php
+++ index.php
@@ -9,33 +9,43 @@
     <?php require("components/head.php"); ?>
 </head>
 <body>
-    <?php require("components/navbar.php") ?>
 
-    <nav class="navbar" id="navbar">
-        <a class="navbar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" href="javascript:void(0);" onclick="toggleFunction()" title="Toggle Navigation Menu">
-            <i class="fa fa-bars"></i>
-        </a>
-        <a href="#home" class="navbar-item w3-button">HOME</a>
-        <a href="#about" class="navbar-item w3-button w3-hide-small"><i class="fa fa-user"></i> ABOUT</a>
-        <a href="#portfolio" class="navbar-item w3-button w3-hide-small"><i class="fa fa-th"></i> PORTFOLIO</a>
-        <a href="#contact" class="navbar-item w3-button w3-hide-small"><i class="fa fa-envelope"></i> CONTACT</a>
-        <a href="#" class="navbar-item w3-button w3-hide-small w3-right w3-hover-red">
-        <i class="fas fa-search"></i>
-        </a>
-    </nav>
+    <!-- <div class="section-header w-100 d-flex justify-content-center">
+        <div class="navbar col-6 justify-content-evenly">
+            <a href="#home" class="navbar-item">HOME</a>
+            <a href="#about" class="navbar-item">ABOUT</a>
+            <a href="#portfolio" class="navbar-item">PORTFOLIO</a>
+            <a href="#contact" class="navbar-item">CONTACT</a>
+        </div>
+    </div>
+
 
     <div class="parallax bgimg-1">
         <div class="text-on-image">
             <span class="border">CLASSE 5BI</span>
         </div>
-    </div>
-    <div class="mydiv p-10">
-        <p class="mytext">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus maiores distinctio eius, molestiae labore deserunt sed aliquid optio ipsa accusamus tempora ut non similique sunt dicta, pariatur laudantium aperiam facilis?</p>
-    </div>
-    <div class="parallax bgimg-2">
-        <div class="text-on-image">
-            <span class="border">CLASSE 5BI</span>
+    </div> -->
+
+    <div class="parallax bgimg-1 d-flex align-items-center">
+        <div class="section-header w-100 d-flex justify-content-center">
+            <div class="navbar col-6 justify-content-evenly">
+                <a href="#home" class="navbar-item">HOME</a>
+                <a href="#about" class="navbar-item">ABOUT</a>
+                <a href="#portfolio" class="navbar-item">PORTFOLIO</a>
+                <a href="#contact" class="navbar-item">CONTACT</a>
+            </div>
         </div>
+    </div>
+
+
+    <div class="description d-flex justify-content-center align-items-center">
+        <div class="text-container col-6 d-flex flex-column">
+            <h2 class="title align-self-center">SEZIONE PROVA</h2>
+            <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus maiores distinctio eius, molestiae labore deserunt sed aliquid optio ipsa accusamus tempora ut non similique sunt dicta, pariatur laudantium aperiam facilis?</p>
+        </div>
+    </div>
+
+    <div class="parallax bgimg-2">
     </div>
     
 </body>
