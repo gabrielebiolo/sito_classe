baf0b99821606c67d989f6e8b07b2b17a7dea7c9
Gabriele Biolo <60276263+gabrielebiolo@users.noreply.github.com>
2021-11-22 20:12:34 +0100

    Progresso importante - Sviluppo sezioni

--- index.php
+++ index.php
@@ -9,8 +9,27 @@
     <?php require("components/head.php"); ?>
 </head>
 <body>
+<!--
+    <div class="main-nav">
+        <a class="main-nav-item" href="">
+            <i class="icon"></i>
+            <span class="tag"></span>
+        </a>
+        <a class="main-nav-item" href="">
+            <i class="icon"></i>
+            <span class="tag"></span>
+        </a>
+        <a class="main-nav-item" href="">
+            <i class="icon"></i>
+            <span class="tag"></span>
+        </a>
+        <a class="main-nav-item" href="">
+            <i class="icon"></i>
+            <span class="tag"></span>
+        </a>
+    </div>
 
-    <!-- <div class="section-header w-100 d-flex justify-content-center">
+    <div class="section-header w-100 d-flex justify-content-center">
         <div class="navbar col-6 justify-content-evenly">
             <a href="#home" class="navbar-item">HOME</a>
             <a href="#about" class="navbar-item">ABOUT</a>
@@ -25,28 +44,82 @@
             <span class="border">CLASSE 5BI</span>
         </div>
     </div> -->
-
-    <div class="parallax bgimg-1 d-flex align-items-center">
+    <div class="parallax bgimg-classe" alt="Classe">
         <div class="section-header w-100 d-flex justify-content-center">
-            <div class="navbar col-6 justify-content-evenly">
-                <a href="#home" class="navbar-item">HOME</a>
-                <a href="#about" class="navbar-item">ABOUT</a>
-                <a href="#portfolio" class="navbar-item">PORTFOLIO</a>
-                <a href="#contact" class="navbar-item">CONTACT</a>
+            <div class="navbar col-6 justify-content-evenly flex-nowrap">
+                <a href="#classe" class="item">
+                    <i class="fas fa-user-graduate"></i>
+                    <span class="label">CLASSE</span>
+                </a>
+                <a href="#docenti" class="item">
+                    <i class="fas fa-chalkboard-teacher"></i>
+                    <span class="label">DOCENTI</span>
+                </a>
+                <a href="#sviluppatori" class="item">
+                    <i class="fas fa-code"></i>
+                    <span class="label">SVILUPPATORI</span>
+                </a>
             </div>
         </div>
     </div>
-
-
-    <div class="description d-flex justify-content-center align-items-center">
+    <section class="description d-flex justify-content-center align-items-center p-5" id="home">
         <div class="text-container col-6 d-flex flex-column">
-            <h2 class="title align-self-center">SEZIONE PROVA</h2>
-            <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus maiores distinctio eius, molestiae labore deserunt sed aliquid optio ipsa accusamus tempora ut non similique sunt dicta, pariatur laudantium aperiam facilis?</p>
+            <h2 class="title align-self-center mb-2">CLASSE</h2>
+            <p class="">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus maiores distinctio eius, molestiae labore deserunt sed aliquid optio ipsa accusamus tempora ut non similique sunt dicta, pariatur laudantium aperiam facilis?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque asperiores, unde repudiandae veritatis voluptatum molestiae nam dolor sequi dolore rem nobis mollitia deserunt aut corporis provident velit consectetur iure soluta?
+            </p>
+        </div>
+    </section>
+
+    <div class="parallax bgimg-docenti" alt="Docente che scrive alla lavagna">
+        <div class="section-header w-100 d-flex justify-content-center">
+            <div class="navbar col-6 justify-content-evenly">
+                <a href="#classe" class="item">CLASSE</a>
+                <a href="#docenti" class="item">DOCENTI</a>
+                <a href="#sviluppatori" class="item">SVILUPPATORI</a>
+            </div>
         </div>
     </div>
+    <section class="description d-flex justify-content-center align-items-center p-5" id="home">
+        <div class="text-container col-6 d-flex flex-column">
+            <h2 class="title align-self-center mb-2">DOCENTI</h2>
+            <p class="">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus maiores distinctio eius, molestiae labore deserunt sed aliquid optio ipsa accusamus tempora ut non similique sunt dicta, pariatur laudantium aperiam facilis?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque asperiores, unde repudiandae veritatis voluptatum molestiae nam dolor sequi dolore rem nobis mollitia deserunt aut corporis provident velit consectetur iure soluta?
+            </p>
+        </div>
+    </section>
 
-    <div class="parallax bgimg-2">
+    <div class="parallax bgimg-sviluppatori" alt="Docente che scrive alla lavagna">
+        <div class="section-header w-100 d-flex justify-content-center">
+            <div class="navbar col-6 justify-content-evenly">
+                <a href="#classe" class="item">CLASSE</a>
+                <a href="#docenti" class="item">DOCENTI</a>
+                <a href="#sviluppatori" class="item">SVILUPPATORI</a>
+            </div>
+        </div>
     </div>
+    <section class="description d-flex justify-content-center align-items-center p-5" id="home">
+        <div class="text-container col-6 d-flex flex-column">
+            <h2 class="title align-self-center mb-2">SVILUPPATORI</h2>
+            <p class="">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus maiores distinctio eius, molestiae labore deserunt sed aliquid optio ipsa accusamus tempora ut non similique sunt dicta, pariatur laudantium aperiam facilis?
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque asperiores, unde repudiandae veritatis voluptatum molestiae nam dolor sequi dolore rem nobis mollitia deserunt aut corporis provident velit consectetur iure soluta?
+            </p>
+        </div>
+    </section>
     
+
+
+
+    <footer class="">
+        <div class="images-credits">
+            <a class="credit freepik-credit" href="https://www.freepik.com/vectors/school">School vector created by upklyak - www.freepik.com</a>
+            <a class="credit freepik-credit" href="https://www.freepik.com/photos/circle">Circle photo created by freepik - www.freepik.com</a>
+            <a class="credit freepik-credit" href='https://www.freepik.com/vectors/banner'>Banner vector created by roserodionova - www.freepik.com</a>
+        </div>
+
+    </footer>
 </body>
 </html>
