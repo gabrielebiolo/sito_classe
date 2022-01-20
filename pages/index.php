d6b8c28828a5c052aed6deb5a44579e2eab45ffc
Gabriele Biolo <60276263+gabrielebiolo@users.noreply.github.com>
2022-01-20 09:34:01 +0100

    update

--- pages/index.php
+++ pages/index.php
@@ -11,57 +11,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-<<<<<<< Updated upstream
-    <div class="main-section-wrapper">
-        <div class="main-subsection-wrapper">
-            <div class="parallax bgimg-classe sticky top-0" alt="Classe">
-                <?php 
-                    printNavbar("CLASSE");
-                ?>
-            </div>
-            <section class="description d-flex justify-content-between align-items-center p-5">
-                <div class="description-image-container">
-                    <img class="description-image col-8" src="../img/undraw_exams_g-4-ow.svg" alt="Insengante">
-                </div>
-                <div class="text-container col-6 d-flex flex-column">
-                    <h2 class="title align-self-center mb-2">CLASSE</h2>
-                    <p class="">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus maiores distinctio eius, molestiae labore deserunt sed aliquid optio ipsa accusamus tempora ut non similique sunt dicta, pariatur laudantium aperiam facilis?
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque asperiores, unde repudiandae veritatis voluptatum molestiae nam dolor sequi dolore rem nobis mollitia deserunt aut corporis provident velit consectetur iure soluta?
-                    </p>
-                </div>
-                <div class="description-image-container">
-                    <img class="description-image col-8" src="../img/undraw_studying_s-3-l7.svg" alt="Insengante">
-                </div>
-            </section>
-        </div>
-
-        <div class="main-subsection-wrapper">
-            <div class="parallax bgimg-docenti sticky top-0" alt="Docente che scrive alla lavagna">
-                <?php 
-                    printNavbar("DOCENTI");
-                ?>            
-            </div>
-            <section class="description d-flex justify-content-between align-items-center p-5">
-                <div class="description-image-container">
-                    <img class="description-image col-8" src="../img/undraw_teaching_f-1-cm.svg" alt="Insengante">
-                </div>
-                <div class="text-container col-6 d-flex flex-column">
-                    <h2 class="title align-self-center mb-2">DOCENTI</h2>
-                    <p class="">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus maiores distinctio eius, molestiae labore deserunt sed aliquid optio ipsa accusamus tempora ut non similique sunt dicta, pariatur laudantium aperiam facilis?
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque asperiores, unde repudiandae veritatis voluptatum molestiae nam dolor sequi dolore rem nobis mollitia deserunt aut corporis provident velit consectetur iure soluta?
-                    </p>
-                </div>
-                <div class="description-image-container">
-                    <img class="description-image col-8" src="../img/undraw_teaching_f-1-cm.svg" alt="Insengante">
-                </div>
-            </section>
-        </div>
-
-        <div class="main-subsection-wrapper">
-            <div class="parallax bgimg-sviluppatori sticky top-0" alt="Docente che scrive alla lavagna">
-=======
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
