9eb4c105a9e5841f5f3b12e04651ef0ce0c9bd3a
Gabriele Biolo <60276263+gabrielebiolo@users.noreply.github.com>
2022-01-26 07:59:25 +0100

    Test updates

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!-- <meta http-equiv="refresh" content="0;url=pages/index.html"> -->
+<title></title>
+<body>
+    <?php
+        echo exec('node js/classeviva.js');
+    ?>
+</body>
+<!-- <script language="javascript">
+    window.location.href = "pages/index.php"
+</script> -->
+</head>
+</html>
---
+++ js/classeviva.js
@@ -0,0 +1,36 @@
+const { Classeviva } = require('../node_modules/classeviva.js');
+
+const classeviva = new Classeviva('', '');
+
+(async () => {
+    await classeviva.login();
+
+    classeviva.getGrades()
+    .then(grades  => {
+        console.log(`Hai un totale di  ${grades.length} voti`);
+        console.log(Object.values(grades));
+
+    });
+
+    classeviva.getCalendar()
+    .then(date => {
+        console.log(`Calendario: `);
+        console.log(Object.values(date));
+
+    });
+
+    classeviva.getNotes()
+    .then(note => {
+        console.log(Object.values(note));
+    });
+
+    classeviva.getSubjects()
+    .then(subject => {
+        console.log(Object.values(subject));
+    });
+
+    setTimeout(() => {
+        classeviva.logout();
+        process.exit();
+    }, 3500);
+})();
